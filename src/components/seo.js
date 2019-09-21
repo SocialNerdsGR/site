de878b77311d21f5417d08b5d939959986cc2fc5
Thanos Korakas <thanoskorakas@gmail.com>
2019-09-21 21:32:44 +0300

    Added og image

--- src/components/seo.js
+++ src/components/seo.js
@@ -58,6 +58,10 @@ function SEO({ description, lang, meta, title }) {
           content: `website`,
         },
         {
+          property: `og:image`,
+          content: `social.png`,
+        },
+        {
           name: `twitter:card`,
           content: `summary`,
         },
--- src/content/nerds/giannisv.md
+++
@@ -1,7 +0,0 @@
----
-link: "https://github.com/jverg"
-stack: "Django, Farmer"
-image: "GV.png"
-order: 3
-type: nerds
----
