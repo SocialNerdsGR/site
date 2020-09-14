0c99cc009c7f3e2d769e340eecb61f91e2f55d0a
Thanos Korakas <thanoskorakas@gmail.com>
2020-09-14 11:36:42 +0300

    Updated readme instructions

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
 
       <div className="home">
         <h1 className="home__title">
-          We will <span>help</span> you to skyrocket your <span>product</span>
+          We will <span>help</span> you skyrocket your <span>product</span>
         </h1>
         <h2 className="home__tagline">We take software seriously</h2>
         <div className="home__actions">
